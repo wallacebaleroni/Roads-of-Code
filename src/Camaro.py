e382c7013cbe1981fba6f650105e32dc5b0876b6
Wallace Baleroni <wallacebaleroni@gmail.com>
2018-10-14 20:03:46 -0300

    Corrections on class initialization

--- src/Camaro.py
+++ src/Camaro.py
@@ -5,9 +5,10 @@ from Vehicle import *
 
 class Camaro(Vehicle):
     def __init__(self, position, velocity=(1, 0), debug=False):
-        image = pygame.image.load("../img/camaro.png")
+        self.image = pygame.image.load("../img/camaro.png")
+        self.image_rect = self.image.get_rect()
 
-        Vehicle.__init__(self, image, position, velocity, debug)
+        Vehicle.__init__(self, self.image, position, velocity, debug)
 
         self.maxspeed = metric_to_pixel(22)
         self.maxaccel = metric_to_pixel(25)
